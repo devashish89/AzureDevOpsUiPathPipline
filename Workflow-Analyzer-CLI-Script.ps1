7be0b4c7e7c5f1c1a7c03db0d11c70d6f8d7aa05
Devashish Nigam <46449047+devashish89@users.noreply.github.com>
2023-06-21 01:40:43 +0530

    Update Workflow-Analyzer-CLI-Script.ps1
    minor change

--- Workflow-Analyzer-CLI-Script.ps1
+++ Workflow-Analyzer-CLI-Script.ps1
@@ -1,9 +1,9 @@
 <# Run UiPath Studio's Workflow Analyzer via command line and pipe results to file #>
 
 param (
-    $ProjectFilePath=".\project.json",
+    $ProjectFilePath="project.json",
     $ExecutableFilePath="C:\Users\"+$env:username+"\AppData\Local\Programs\UiPath\Studio\UiPath.Studio.CommandLine.exe",
-    $OutputFilePath=".\$(Get-Date -Format 'yyyy-MM-dd-HH-mm-ss')-Workflow-Analysis.json"
+    $OutputFilePath="$(Get-Date -Format 'yyyy-MM-dd-HH-mm-ss')-Workflow-Analysis.json"
     )
 
 Write-Output "$(Get-Date -Format 'HH:mm:ss') - STARTED - Workflow Analyzer CLI Script"
